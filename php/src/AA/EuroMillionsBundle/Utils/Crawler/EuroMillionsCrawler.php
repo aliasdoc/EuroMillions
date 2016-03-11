1001e3b36149021fc35345c921d07557ab46b19c
Artur Alves <artur.alves@gatewit.com>
2016-03-11 14:53:32 +0000

    Added the functional test UpdateDrawsCommandTest. Moved the crawler's logic to a new class

---
+++ php/src/AA/EuroMillionsBundle/Utils/Crawler/EuroMillionsCrawler.php
@@ -0,0 +1,51 @@
+<?php
+namespace AA\EuroMillionsBundle\Utils\Crawler;
+
+class EuroMillionsCrawler extends Crawler
+{
+    /**
+     * Sets the draw's date
+     *
+     * @author Artur Alves <artur.alves@gatewit.com>
+     *
+     * @param  int $date An Unix timestamp of the draw's date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Executes the crawler and gets the draw's results
+     *
+     * @author Artur Alves <artur.alves@gatewit.com>
+     *
+     * @return array|null The draw's results or null if the date is not set
+     */
+    public function crawl()
+    {
+        if (!isset($this->date)) {
+            return null;
+        }
+
+        $url = "http://pt.euro-millions.com/resultados/" . date('d-m-Y', $this->date);
+        $domDomcument = $this->getContentFrom($url, true);
+
+        $numberWraperDomNode = $domDomcument->getElementById('jsBallOrderCell');
+        $numbersDomNodes = $numberWraperDomNode->getElementsByTagName('li');
+
+        $crawlResults = array(
+            "numbers" => array(),
+            "stars" => array(),
+        );
+        foreach ($numbersDomNodes as $key => $numberDomNode) {
+            if ($key < 5) {
+                $crawlResults['numbers'][] = $numberDomNode->nodeValue;
+            } else {
+                $crawlResults['stars'][] = $numberDomNode->nodeValue;
+            }
+        }
+
+        return $crawlResults;
+    }
+}
