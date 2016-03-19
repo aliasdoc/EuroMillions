5becca4bc228a0185b7be86ceb7cb036442ddd27
Artur Alves <artur.ze.alves@gmail.com>
2016-03-19 15:39:06 +0100

    Fixed data fixtures

--- php/src/ArturAlves/EuroMillionsBundle/DataFixtures/ORM/LoadDrawData.php
+++ php/src/ArturAlves/EuroMillionsBundle/DataFixtures/ORM/LoadDrawData.php
@@ -10,13 +10,7 @@ class LoadDrawData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $draw = new Draw();
-        $draw->setNumber1('4');
-        $draw->setNumber2('37');
-        $draw->setNumber3('38');
-        $draw->setNumber4('39');
-        $draw->setNumber5('44');
-        $draw->setStar1('4');
-        $draw->setStar2('7');
+        $draw->setResult('{"numbers":[4,37,38,39,44],"stars":[4,7]}');
         $draw->setDate("2016-01-01");
 
         $manager->persist($draw);
